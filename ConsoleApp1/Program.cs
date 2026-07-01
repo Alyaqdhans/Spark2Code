87c059bdbf1bc56222e0cebac91505d0034e8d83
xQudo <alyaqdhan690s@gmail.com>
2026-07-01 10:05:29 +0400

    blank

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter your salary: ");
+            float salary = float.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Welcome back " + name + ", " + age + " years old.");
+            Console.WriteLine("Your salary: " + salary);
         }
     }
 }
---
+++ Csharp Fundamentals - Part 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_Fundamentals___Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
