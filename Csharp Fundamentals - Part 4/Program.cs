895f87f6165931aecbaac9c3f6f2d206a6b9bdd3
xQudo <alyaqdhan690s@gmail.com>
2026-07-06 19:37:41 +0400

    Solve Task 1

---
+++ Csharp Fundamentals - Part 4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Csharp_Fundamentals___Part_4
+{
+    internal class Program
+    {
+        static void PrintWelcome(string username)
+        {
+            Console.WriteLine("Welcome back, " + username);
+        }
+
+        static void Main(string[] args)
+        {
+            //Task 1 - Personalized Welcome Function
+            Console.Write("Enter your username: ");
+            PrintWelcome(Console.ReadLine()!);
+
+        }
+    }
+}
