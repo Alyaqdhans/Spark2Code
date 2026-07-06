2e9bcea4534349ea3daad9a11d2d7d407b830227
xQudo <alyaqdhan690s@gmail.com>
2026-07-06 19:39:23 +0400

    Solve Task 2

--- Csharp Fundamentals - Part 4/Program.cs
+++ Csharp Fundamentals - Part 4/Program.cs
@@ -7,12 +7,22 @@
             Console.WriteLine("Welcome back, " + username);
         }
 
+        static int Square(int number)
+        {
+            return number * number;
+        }
+
         static void Main(string[] args)
         {
             //Task 1 - Personalized Welcome Function
             Console.Write("Enter your username: ");
             PrintWelcome(Console.ReadLine()!);
 
+
+            //Task 2 - Square Number Function
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("The square is " + Square(number));
         }
     }
 }
