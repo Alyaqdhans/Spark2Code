109a7e49674605057414bb41a25a4a54cee59547
xQudo <alyaqdhan690s@gmail.com>
2026-07-08 00:19:42 +0400

    Solve Task 10

--- Csharp Fundamentals - Part 5/Program.cs
+++ Csharp Fundamentals - Part 5/Program.cs
@@ -4,148 +4,148 @@
     {
         static void Main(string[] args)
         {
-            ////Task 1 - Fixed Grades Array
-            //int[] nums = new int[5];
+            //Task 1 - Fixed Grades Array
+            int[] nums = new int[5];
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"Enter grade {i + 1}: ");
-            //    int grade = int.Parse(Console.ReadLine()!);
-            //    nums[i] = grade;
-            //}
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter grade {i + 1}: ");
+                int grade = int.Parse(Console.ReadLine()!);
+                nums[i] = grade;
+            }
 
-            //foreach (int num in nums)
-            //{
-            //    Console.WriteLine(num);
-            //}
-
-
-            ////Task 2 - Dynamic To - Do List
-            //List<string> toDo = new List<string>();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"Enter task {i + 1}: ");
-            //    string task = Console.ReadLine()!;
-            //    toDo.Add(task);
-            //}
-
-            //foreach (string task in toDo)
-            //{
-            //    Console.WriteLine(task);
-            //}
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
 
 
-            ////Task 3 - Browsing History Stack
-            //Stack<string> history = new Stack<string>();
+            //Task 2 - Dynamic To - Do List
+            List<string> toDo = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter task {i + 1}: ");
+                string task = Console.ReadLine()!;
+                toDo.Add(task);
+            }
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Console.Write($"Enter URL {i + 1}: ");
-            //    string url = Console.ReadLine()!;
-            //    history.Push(url);
-            //}
-
-            //Console.WriteLine(history.Pop());
-
-
-            ////Task 4 - Customer Service Queue
-            //Queue<string> customers = new Queue<string>();
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Console.Write($"Enter customer name {i + 1}: ");
-            //    string customer = Console.ReadLine()!;
-            //    customers.Enqueue(customer);
-            //}
-
-            //Console.WriteLine("Serving customer: " + customers.Dequeue());
+            foreach (string task in toDo)
+            {
+                Console.WriteLine(task);
+            }
 
 
-            ////Task 5 - Array Grade Range
-            //int[] grades = new int[5];
+            //Task 3 - Browsing History Stack
+            Stack<string> history = new Stack<string>();
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"Enter grade {i + 1}: ");
-            //    int grade = int.Parse(Console.ReadLine()!);
-            //    grades[i] = grade;
-            //}
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter URL {i + 1}: ");
+                string url = Console.ReadLine()!;
+                history.Push(url);
+            }
 
-            //grades.Sort();
-
-            //Console.WriteLine("Lowest grade: " + grades[0]);
-            //Console.WriteLine("Highest grade: " + grades[4]);
-            //Console.WriteLine("Average grade: " + grades.Sum() / 5);
+            Console.WriteLine(history.Pop());
 
 
-            ////Task 6 - Filtered Shopping List
-            //List<string> shoppingList = new List<string>();
+            //Task 4 - Customer Service Queue
+            Queue<string> customers = new Queue<string>();
 
-            //string shoppingLoop = "";
-            //while (shoppingLoop != "done")
-            //{
-            //    Console.Write("Enter item: ");
-            //    shoppingLoop = Console.ReadLine()!;
-            //    if (shoppingLoop != "done")
-            //    {
-            //        shoppingList.Add(shoppingLoop);
-            //    }
-            //}
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter customer name {i + 1}: ");
+                string customer = Console.ReadLine()!;
+                customers.Enqueue(customer);
+            }
 
-            //foreach (string customer in shoppingList)
-            //{
-            //    Console.WriteLine(customer);
-            //}
-
-            //Console.Write("Enter item to remove: ");
-            //string itemToRemove = Console.ReadLine()!;
-            //shoppingList.Remove(itemToRemove);
-
-            //foreach (string customer in shoppingList)
-            //{
-            //    Console.WriteLine(customer);
-            //}
+            Console.WriteLine("Serving customer: " + customers.Dequeue());
 
 
-            ////Task 7 - High Score Podium
-            //List<int> scores = new List<int>();
+            //Task 5 - Array Grade Range
+            int[] grades = new int[5];
 
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"Enter score {i + 1}: ");
-            //    int score = int.Parse(Console.ReadLine()!);
-            //    scores.Add(score);
-            //}
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter grade {i + 1}: ");
+                int grade = int.Parse(Console.ReadLine()!);
+                grades[i] = grade;
+            }
 
-            //scores.Sort();
-            //scores.Reverse();
+            grades.Sort();
 
-            //Console.WriteLine("1st place: " + scores[0]);
-            //Console.WriteLine("2nd place: " + scores[1]);
-            //Console.WriteLine("3rd place: " + scores[2]);
+            Console.WriteLine("Lowest grade: " + grades[0]);
+            Console.WriteLine("Highest grade: " + grades[4]);
+            Console.WriteLine("Average grade: " + grades.Sum() / 5);
 
 
-            ////Task 8 - Undo Last Action
-            //Stack<string> actions = new Stack<string>();
-            
-            //string actionLoop = "";
-            //while (actionLoop != "stop")
-            //{
-            //    Console.Write("Enter action: ");
-            //    actionLoop = Console.ReadLine()!;
-            //    if (actionLoop != "stop")
-            //    {
-            //        actions.Push(actionLoop);
-            //    }
-            //}
+            //Task 6 - Filtered Shopping List
+            List<string> shoppingList = new List<string>();
 
-            //Console.WriteLine("Undoing last action: " + actions.Pop());
-            //Console.WriteLine("Undoing last action: " + actions.Pop());
+            string shoppingLoop = "";
+            while (shoppingLoop != "done")
+            {
+                Console.Write("Enter item: ");
+                shoppingLoop = Console.ReadLine()!;
+                if (shoppingLoop != "done")
+                {
+                    shoppingList.Add(shoppingLoop);
+                }
+            }
 
-            //foreach (string action in actions)
-            //{
-            //    Console.WriteLine(action);
-            //}
+            foreach (string customer in shoppingList)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.Write("Enter item to remove: ");
+            string itemToRemove = Console.ReadLine()!;
+            shoppingList.Remove(itemToRemove);
+
+            foreach (string customer in shoppingList)
+            {
+                Console.WriteLine(customer);
+            }
+
+
+            //Task 7 - High Score Podium
+            List<int> scores = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter score {i + 1}: ");
+                int score = int.Parse(Console.ReadLine()!);
+                scores.Add(score);
+            }
+
+            scores.Sort();
+            scores.Reverse();
+
+            Console.WriteLine("1st place: " + scores[0]);
+            Console.WriteLine("2nd place: " + scores[1]);
+            Console.WriteLine("3rd place: " + scores[2]);
+
+
+            //Task 8 - Undo Last Action
+            Stack<string> actions = new Stack<string>();
+
+            string actionLoop = "";
+            while (actionLoop != "stop")
+            {
+                Console.Write("Enter action: ");
+                actionLoop = Console.ReadLine()!;
+                if (actionLoop != "stop")
+                {
+                    actions.Push(actionLoop);
+                }
+            }
+
+            Console.WriteLine("Undoing last action: " + actions.Pop());
+            Console.WriteLine("Undoing last action: " + actions.Pop());
+
+            foreach (string action in actions)
+            {
+                Console.WriteLine(action);
+            }
 
 
             //Task 9 - Grade Analyzer with Functions
@@ -182,6 +182,49 @@
             else
             {
                 Console.WriteLine("First failing grade: " + firstFailingGrade);
+            }
+
+
+            //Task 10 - Print Queue Manager
+            static Queue<string> RemoveJob(Queue<string> printQueue, string jobToRemove)
+            {
+                Queue<string> updatedQueue = new Queue<string>();
+                while (printQueue.Count > 0)
+                {
+                    string currentJob = printQueue.Dequeue();
+                    if (currentJob != jobToRemove)
+                    {
+                        updatedQueue.Enqueue(currentJob);
+                    }
+                }
+                return updatedQueue;
+            }
+
+            Queue<string> prints = new Queue<string>();
+
+            string printLoop = "";
+            while (printLoop != "done")
+            {
+                Console.Write("Enter print job: ");
+                printLoop = Console.ReadLine()!;
+                if (printLoop != "done")
+                {
+                    prints.Enqueue(printLoop);
+                }
+            }
+
+            foreach (string job in prints)
+            {
+                Console.WriteLine(job);
+            }
+
+            Console.Write("Enter a job to remove: ");
+            string jobToRemove = Console.ReadLine()!;
+
+            Queue<string> updatedPrints = RemoveJob(prints, jobToRemove);
+            foreach (string job in updatedPrints)
+            {
+                Console.WriteLine(job);
             }
         }
     }
