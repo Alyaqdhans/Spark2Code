bc34b48652bb19d00d7d6a5016dbeb2fa9a7db48
xQudo <alyaqdhan690s@gmail.com>
2026-07-07 22:50:06 +0400

    Solve Task 1

---
+++ Csharp Fundamentals - Part 5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Csharp_Fundamentals___Part_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Fixed Grades Array
+            int[] nums = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter grade {i+1}: ");
+                int grade = int.Parse(Console.ReadLine()!);
+                nums[i] = grade;
+            }
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
