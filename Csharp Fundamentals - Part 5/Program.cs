df5fdd3359e38a4b1d39c71b4fbee2ae26226f22
xQudo <alyaqdhan690s@gmail.com>
2026-07-07 22:56:01 +0400

    Solve Task 2

--- Csharp Fundamentals - Part 5/Program.cs
+++ Csharp Fundamentals - Part 5/Program.cs
@@ -9,7 +9,7 @@
 
             for (int i = 0; i < 5; i++)
             {
-                Console.Write($"Enter grade {i+1}: ");
+                Console.Write($"Enter grade {i + 1}: ");
                 int grade = int.Parse(Console.ReadLine()!);
                 nums[i] = grade;
             }
@@ -17,6 +17,21 @@
             foreach (int num in nums)
             {
                 Console.WriteLine(num);
+            }
+
+
+            //Task 2 - Dynamic To - Do List
+            List<string> toDo = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter task {i + 1}: ");
+                string task = Console.ReadLine()!;
+                toDo.Add(task);
+            }
+
+            foreach (string task in toDo)
+            {
+                Console.WriteLine(task);
             }
         }
     }
