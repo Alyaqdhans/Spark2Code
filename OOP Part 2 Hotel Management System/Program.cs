90b5c89da6469cd5fd0be49eb319bfcdfd7190a7
xQudo <alyaqdhan690s@gmail.com>
2026-07-14 17:04:06 +0400

    create Room Class

---
+++ OOP Part 2 Hotel Management System/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace OOP_Part_2_Hotel_Management_System
+{
+    class Room
+    {
+        int roomNumber;
+        string roomType;
+        double pricePerNight;
+        bool isAvailable;
+
+        public Room(int RoomNumber, string RoomType, double PricePerNight, bool IsAvailable)
+        {
+            roomNumber = RoomNumber;
+            roomType = RoomType;
+            pricePerNight = PricePerNight;
+            isAvailable = IsAvailable;
+        }
+
+        public void displayRoom()
+        {
+            Console.WriteLine($"Room Number: {roomNumber}");
+            Console.WriteLine($"Room Type: {roomType}");
+            Console.WriteLine($"Price Per Night: {pricePerNight}");
+            Console.WriteLine($"Is Available: {isAvailable}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
